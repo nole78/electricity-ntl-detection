a8131f207580655e80b91da4f87f7ba87fa54edd
milanmilosev1 <milosevmil@gmail.com>
2026-04-18 14:06:00 +0200

    Implemented all repositories needed for frontend

---
+++ server/Hack2on/Hack2on/Core/Abstractions/ITransmissionStationRepository.cs
@@ -0,0 +1,9 @@
+﻿using Hack2on.Core.Entities;
+
+namespace Hack2on.Core.Abstractions
+{
+    public interface ITransmissionStationRepository
+    {
+        Task<IReadOnlyList<TransmissionStation>> GetAllTransmissionStationsAsync(CancellationToken ct);
+    }
+}
---
+++ server/Hack2on/Hack2on/Infrastructure/RepositoryImplementations/DistributionSubstationRepository.cs
@@ -0,0 +1,21 @@
+﻿using Hack2on.Core.Abstractions;
+using Hack2on.Core.Entities;
+using Dapper;
+using Microsoft.Data.SqlClient;
+
+namespace Hack2on.Infrastructure.RepositoryImplementations
+{
+    public class DistributionSubstationRepository(string connectionString) : IDistributionSubstationRepository
+    {
+        private readonly string _connectionString = connectionString;
+
+        public async Task<IReadOnlyList<DistributionSubstation>> GetAllDistributionSubstationAsync(CancellationToken ct = default)
+        {
+            using var connection = new SqlConnection(_connectionString);
+            await connection.OpenAsync(ct);
+            const string sql = "SELECT Id, Name, MeterId, Feeder11Id, Feeder33Id, NameplateRating, Latitude, Longitude FROM DistributionSubstation ORDER BY Name";
+            var results = await connection.QueryAsync<DistributionSubstation>(sql);
+            return results.ToList().AsReadOnly();
+        }
+    }
+}
