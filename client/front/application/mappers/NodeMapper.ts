bd767895a9095845996db4ba0940295c9bc45b54
milan-bitcube <milan@bitcube.tech>
2026-04-18 14:17:47 +0200

    Changed nodeMapper to not use position

--- client/front/application/mappers/NodeMapper.ts
+++ client/front/application/mappers/NodeMapper.ts
@@ -1,13 +1,11 @@
 import { DtDTO } from "../dto/DtDTO";
 import { SubstationDTO } from "../dto/SubstationDTO";
 import { TransmissionStationDTO } from "../dto/TransmissionStationDTO";
-import { ProjectionService } from "../services/ProjectionService";
 import { StationNode } from "@/domain/models/StationNode";
 
 export class NodeMapper {
 
   static fromTransmission(dto: TransmissionStationDTO): StationNode {
-  const pos = ProjectionService.project(dto.latitude, dto.longitude);
 
   return {
     id: `TS-${dto.id}`,
@@ -15,35 +13,29 @@ export class NodeMapper {
     type: "TS",
 
     latitude: dto.latitude,    
-    longitude: dto.longitude,   
-
-    position: pos
+    longitude: dto.longitude   
   };
 }
 
   static fromSubstation(dto: SubstationDTO): StationNode {
-    const pos = ProjectionService.project(dto.latitude, dto.longitude);
 
     return {
       id: `SS-${dto.id}`,
       name: dto.name,
       type: "SS",
       latitude: dto.latitude,
-      longitude: dto.longitude,
-      position: pos
+      longitude: dto.longitude
     };
   }
 
   static fromDt(dto: DtDTO): StationNode {
-    const pos = ProjectionService.project(dto.latitude, dto.longitude);
 
     return {
       id: `DT-${dto.id}`,
       name: dto.name,
       type: "DT",
       latitude: dto.latitude,
-      longitude: dto.longitude,
-      position: pos
+      longitude: dto.longitude
     };
   }
 }
