30ed9d9b905094018741b613fb18d40cf15e5039
milanmilosev1 <milosevmil@gmail.com>
2026-04-18 16:02:05 +0200

    substation controller

---
+++ server/Hack2on/Hack2on/Core/DTOs/SubstationDTOs/GetAllSubstationsDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Hack2on.Core.DTOs.SubstationDTOs
+{
+    public class GetAllSubstationsDTO
+    {
+        public string? Name { get; set; }
+        public decimal? Latitude { get; set; }
+        public decimal? Longitude { get; set; }
+    }
+}
